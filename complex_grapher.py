70c7c4328cbb14dfe7ef96615ca48f17a3dd900f
VinayR-GitHub <153238516+VinayR-GitHub@users.noreply.github.com>
2023-12-08 10:41:35 +1100

    Update complex_grapher.py

--- complex_grapher.py
+++ complex_grapher.py
@@ -4,9 +4,19 @@
 import numpy as np
 import matplotlib as mpl
 
-def colour_domain(a, dim_x, dim_y, f):
-  """{f} is the function, {a} is the acuity, {dim_x} is the size in x, and {dim_y} is the size in y."""
-  x.s, x.e, x.r, y.s, y.e, y.r = -a, a, dim_x, -a, a, dim_y
-  x, y = np.ogrid[x.s : x.e : (1j * x.r), y.s : y.e : (1j * y.r)]
-  plt.imshow(np.angle(f((x - (1j * y)).T)))
-  plt.show()
+def domain_colour(a, dim_x, dim_y, f):
+    """{f} is the function, {a} is the acuity, {dim_x} is the size in x, and {dim_y} is the size in y."""
+    x.s, x.e, x.r, y.s, y.e, y.r = -a, a, dim_x, -a, a, dim_y
+    x, y = np.ogrid[x.s : x.e : (1j * x.r), y.s : y.e : (1j * y.r)]
+    plt.imshow(np.angle(f((x - (1j * y)).T)))
+    plt.show()
+
+def user_function():
+    return lambda z: eval(input("f(z) = "))
+
+def user_graph():
+    u.a = input("Acuity: ")
+    u.dim_x = input("x-Dimension: ")
+    u.dim_y = input("y-Dimension: ")
+    u.f = user_function()
+    domain_colour(u.a, u.dim_x, u.dim_y, u.f)
