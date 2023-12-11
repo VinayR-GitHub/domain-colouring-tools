dab4e4657bf3fafccaa714dfce66c07a64211e26
VinayR-GitHub <153238516+VinayR-GitHub@users.noreply.github.com>
2023-12-11 14:55:55 +1100

    Update domain_colouring_continuous.py
    Added a function evaluation mechanism.

--- graphing/domain_colouring_continuous.py
+++ graphing/domain_colouring_continuous.py
@@ -16,3 +16,23 @@ def absolute_grading(z):
     return grad(
         np.absolute(z)
     )
+
+def eval_func(f, dim_Re, dim_Im, a):
+    """Where f is a function of z, dim_Re and dim_Im are tuples outlining the rectangular graphing domain, and A represents the number of points in each unit interval."""
+    hei = dim_Im [1] - dim_Im [0]
+    len = dim_Re [1] - dim_Re [0]
+    h_res = A * hei
+    l_res = A * len
+    x = np.linspace(
+        dim_Re [0],
+        dim_Re [1],
+        int(l_res)
+    )
+    y = np.linspace(
+        dim_Im [0],
+        dim_Im [1],
+        int(h_res)
+    )
+    x, y = np.meshgrid(x, y)
+    z = x + (1j * y)
+    return f(z)
