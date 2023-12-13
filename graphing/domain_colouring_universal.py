0958c7140e7fc376a4ebb2052ecaec4747f97c22
VinayR-GitHub <153238516+VinayR-GitHub@users.noreply.github.com>
2023-12-13 11:30:40 +1100

    Update domain_colouring_universal.py
    Integrated universal colouring method.

--- graphing/domain_colouring_universal.py
+++ graphing/domain_colouring_universal.py
@@ -51,144 +51,6 @@ def colour_map_co(vals, sat):
     )
     return colsys.hsv_to_rgb(col_hsv)
 
-def domain_plot(colmap, f, dim_Re, dim_Im, title = "", sat = 1, A = 500):
-    """Where {f} is a function over z, {dim_Re} and {dim_Im} outline the dimensions of the rectangular domain, {sat} represents saturation, and {A} represents unit interval acuity."""
-    vals = eval_func(f, dim_Re, dim_Im, A)
-    cols = colmap(vals, sat)
-    mpl.xlabel("$\Re(z)$")
-    mpl.ylabel("$\Im(z)$")
-    mpl.title(title)
-    mpl.imshow(
-        cols,
-        origin = "lower",
-        extent = [
-            dim_Re [0],
-            dim_Re [1],
-            dim_Im [0],
-            dim_Im [1]
-        ]
-    )
-
-def pairview(dim_Re, dim_Im, colmap, f, title = "", z_dim = [-10, 10, -10, 10], sat = 1, A = 500):
-    """Where {f} is a function over z, {dim_Re}, {dim_Im}, and {z_dim} outline the dimensions of the rectangular domain, {sat} represents saturation, {A} represents unit interval acuity."""
-    mpl.rcParams['figure.figsize'] = 8, 5
-    mpl.subplot(1, 2, 1)
-    domain_plot(
-        colmap,
-        f,
-        dim_Re,
-        dim_Im,
-        title,
-        sat,
-        A
-    )
-    mpl.subplot(1, 2, 2)
-    domain_plot(
-        colmap,
-        identity := lambda z: z,
-        [z_dim [0], z_dim [1]],
-        [z_dim [2], z_dim [3]],
-        "$f(z)=z$",
-        sat,
-        A
-    )
-    mpl.tight_layout()
-
-def file_settings():
-    if bool(
-        input("Save file: ")
-    ) == False:
-        return [
-            None,
-            None
-        ]
-    else:
-        file_name = input("File name: ")
-        file_type = "." + input("File type: ") #Do not use full stop in file type.
-    return [
-        file_name,
-        file_type
-    ]
-
-def user_mapping(colmap):
-    """Where {colmap} defines a colour mapping schema."""
-    func = input("f(z) = ")
-    f = lambda z: eval(
-        func
-    )
-    title = f"${f'f(z)={func}'.replace('**', '^')}$"
-    sat = float(
-        input("Saturation: ")
-    )
-    A = int(
-        input("Resolution/acuity: ")
-    )
-    dim_Re = [
-        float(
-            input("Lower real boundary: ")
-        ),
-        float(
-            input("Upper real boundary: ")
-        )
-    ]
-    dim_Im = [
-        float(
-            input("Lower imaginary boundary: ")
-        ),
-        float(
-            input("Upper imaginary boundary: ")
-        )
-    ]
-    pair = bool(
-        input("Pair viewing: ") #Takes only True/False.
-    )
-    if pair == False:
-        pass
-    elif pair == True:
-        z_dim = [
-            float(
-                input("Lower real boundary for identity: ")
-            ),
-            float(
-                input("Upper real boundary for identity: ")
-            ),
-            float(
-                input("Lower imaginary boundary for identity: ")
-            ),
-            float(
-                input("Upper imaginary boundary for identity: ")
-            )
-        ]
-    if pair == False:
-        domain_plot(
-            colmap,
-            f,
-            dim_Re,
-            dim_Im,
-            title,
-            sat,
-            A
-        )
-    elif pair == True:
-        pairview(
-            dim_Re,
-            dim_Im,
-            colmap,
-            f,
-            title,
-            z_dim,
-            sat,
-            A
-        )
-    settings = file_settings()
-    if settings [0] == None:
-        mpl.show()
-    else:
-        mpl.savefig(settings [0] + settings [1])
-        mpl.show()
-
-user_mapping(colour_map)
-
 def colour_map_wm(vals, sat, power = 2):
     """{vals} represents a dataset of values, {sat} represents the colour saturation in the HSV system, and {power} represents the base of the contour plot schema."""
     h_vals = hue(vals)
@@ -208,35 +70,59 @@ def colour_map_wm(vals, sat, power = 2):
     )
     return colsys.hsv_to_rgb(col_hsv)
 
-def pairview(dim_Re, dim_Im, colmap, f, title = "", z_dim = [-10, 10, -10, 10], sat = 1, A = 500, power = 2):
-    """Where {f} is a function over z, {dim_Re}, {dim_Im}, and {z_dim} outline the dimensions of the rectangular domain, {sat} represents saturation, {A} represents unit interval acuity."""
-    mpl.rcParams['figure.figsize'] = 8, 5
-    mpl.subplot(1, 2, 1)
-    domain_plot(
-        colmap,
-        f,
-        dim_Re,
-        dim_Im,
-        title,
-        sat,
-        A,
-        power
+def colour_map_ph(vals, sat):
+    """{vals} represents a dataset of values, {sat} represents the colour saturation in the HSV system."""
+    h_vals = hue(vals)
+    m_val = 0.7
+    M_val = 1
+    n_val = 15 #Optimal isochromatic quantity for analytic and conformal mappings.
+    phase_func = lambda a, b, c, d: c + ((d - c) * ((a / b) - np.floor(a / b)))
+    v_vals = phase_func(h_vals, (1.0 / n), m, M) * phase_func(
+        np.nan_to_num(
+            np.log(
+                np.absolute(vals)
+            )
+        ),
+        (2 * np.pi / n),
+        m,
+        M
     )
-    mpl.subplot(1, 2, 2)
-    domain_plot(
-        colmap,
-        identity := lambda z: z,
-        [z_dim [0], z_dim [1]],
-        [z_dim [2], z_dim [3]],
-        "$f(z)=z$",
-        sat,
-        A,
-        power
+    s_vals = sat * np.ones(
+        h_vals.shape
     )
-    mpl.tight_layout()
+    col_hsv = np.dstack(
+        (h_vals, s_vals, v_vals)
+    )
+    return colsys.hsv_to_rgb(col_hsv)
 
-def user_mapping(colmap):
-    """Where {colmap} defines a colour mapping schema."""
+def domain_plot(colmap, f, dim_Re, dim_Im, title = "", sat = 1, A = 500):
+    """Where {f} is a function over z, {dim_Re} and {dim_Im} outline the dimensions of the rectangular domain, {sat} represents saturation, and {A} represents unit interval acuity."""
+    vals = eval_func(f, dim_Re, dim_Im, A)
+    cols = colmap(vals, sat)
+    mpl.xlabel("$\Re(z)$")
+    mpl.ylabel("$\Im(z)$")
+    mpl.title(title)
+    mpl.imshow(
+        cols,
+        origin = "lower",
+        extent = [
+            dim_Re [0],
+            dim_Re [1],
+            dim_Im [0],
+            dim_Im [1]
+        ]
+    )
+
+def file_settings():
+    file_name = input("File name: ")
+    file_type = "." + input("File type: ") #Do not use full stop in file type.
+    return [
+        file_name,
+        file_type
+    ]
+
+def user_mapping(colmap_co, colmap_wm, colmap_ph):
+    """Where {colmap} variants define a colour mapping schema."""
     func = input("f(z) = ")
     f = lambda z: eval(
         func
@@ -267,99 +153,53 @@ def user_mapping(colmap):
             input("Upper imaginary boundary: ")
         )
     ]
-    pair = bool(
-        input("Pair viewing: ") #Takes only True/False.
-    )
-    if pair == False:
-        pass
-    elif pair == True:
-        z_dim = [
-            float(
-                input("Lower real boundary for identity: ")
-            ),
-            float(
-                input("Upper real boundary for identity: ")
-            ),
-            float(
-                input("Lower imaginary boundary for identity: ")
-            ),
-            float(
-                input("Upper imaginary boundary for identity: ")
-            )
-        ]
-    if pair == False:
-        domain_plot(
-            colmap,
-            f,
-            dim_Re,
-            dim_Im,
-            title,
-            sat,
-            A,
-            power
-        )
-    elif pair == True:
-        pairview(
-            dim_Re,
-            dim_Im,
-            colmap,
-            f,
-            title,
-            z_dim,
-            sat,
-            A,
-            power
-        )
-    settings = file_settings()
-    if settings [0] == None:
-        mpl.show()
-    else:
-        mpl.savefig(settings [0] + settings [1])
-        mpl.show()
-
-user_mapping(colour_map_wm)
-
-def colour_map_ph(vals, sat):
-    """{vals} represents a dataset of values, {sat} represents the colour saturation in the HSV system."""
-    h_vals = hue(vals)
-    m_val = 0.7
-    M_val = 1
-    n_val = 15 #Optimal isochromatic quantity for analytic and conformal mappings.
-    phase_func = lambda a, b, c, d: c + ((d - c) * ((a / b) - np.floor(a / b)))
-    v_vals = phase_func(h_vals, (1.0 / n), m, M) * phase_func(
-        np.nan_to_num(
-            np.log(
-                np.absolute(vals)
-            )
+    z_dim = [
+        float(
+            input("Lower real boundary for identity: ")
         ),
-        (2 * np.pi / n),
-        m,
-        M
-    )
-    s_vals = sat * np.ones(
-        h_vals.shape
-    )
-    col_hsv = np.dstack(
-        (h_vals, s_vals, v_vals)
-    )
-    return colsys.hsv_to_rgb(col_hsv)
-
-def pairview(dim_Re, dim_Im, colmap, f, title = "", z_dim = [-10, 10, -10, 10], sat = 1, A = 500):
-    """Where {f} is a function over z, {dim_Re}, {dim_Im}, and {z_dim} outline the dimensions of the rectangular domain, {sat} represents saturation, {A} represents unit interval acuity."""
-    mpl.rcParams['figure.figsize'] = 8, 5
-    mpl.subplot(1, 2, 1)
+        float(
+            input("Upper real boundary for identity: ")
+        ),
+        float(
+            input("Lower imaginary boundary for identity: ")
+        ),
+        float(
+            input("Upper imaginary boundary for identity: ")
+        )
+    ]
+    mpl.subplot(2, 3, 1)
     domain_plot(
-        colmap,
+        colmap_co,
         f,
         dim_Re,
         dim_Im,
-        title,
+        "$f(z)=z$",
         sat,
         A
     )
-    mpl.subplot(1, 2, 2)
+    plt.subplot(2, 3, 2)
     domain_plot(
-        colmap,
+        colmap_wm,
+        f,
+        dim_Re,
+        dim_Im,
+        "$f(z)=z$",
+        sat,
+        A
+    )
+    plt.subplot(2, 3, 3)
+    domain_plot(
+        colmap_ph,
+        f,
+        dim_Re,
+        dim_Im,
+        "$f(z)=z$",
+        sat,
+        A
+    )
+    mpl.subplot(2, 3, 4)
+    domain_plot(
+        colmap_co,
         identity := lambda z: z,
         [z_dim [0], z_dim [1]],
         [z_dim [2], z_dim [3]],
@@ -367,83 +207,33 @@ def pairview(dim_Re, dim_Im, colmap, f, title = "", z_dim = [-10, 10, -10, 10], 
         sat,
         A
     )
-    mpl.tight_layout()
-
-def user_mapping(colmap):
-    """Where {colmap} defines a colour mapping schema."""
-    func = input("f(z) = ")
-    f = lambda z: eval(
-        func
+    plt.subplot(2, 3, 5)
+    domain_plot(
+        colmap_wm,
+        identity := lambda z: z,
+        [z_dim [0], z_dim [1]],
+        [z_dim [2], z_dim [3]],
+        "$f(z)=z$",
+        sat,
+        A
     )
-    title = f"${f'f(z)={func}'.replace('**', '^')}$"
-    sat = float(
-        input("Saturation: ")
+    plt.subplot(2, 3, 6)
+    domain_plot(
+        colmap_ph,
+        identity := lambda z: z,
+        [z_dim [0], z_dim [1]],
+        [z_dim [2], z_dim [3]],
+        "$f(z)=z$",
+        sat,
+        A
     )
-    A = int(
-        input("Resolution/acuity: ")
-    )
-    dim_Re = [
-        float(
-            input("Lower real boundary: ")
-        ),
-        float(
-            input("Upper real boundary: ")
-        )
-    ]
-    dim_Im = [
-        float(
-            input("Lower imaginary boundary: ")
-        ),
-        float(
-            input("Upper imaginary boundary: ")
-        )
-    ]
-    pair = bool(
-        input("Pair viewing: ") #Takes only True/False.
-    )
-    if pair == False:
-        pass
-    elif pair == True:
-        z_dim = [
-            float(
-                input("Lower real boundary for identity: ")
-            ),
-            float(
-                input("Upper real boundary for identity: ")
-            ),
-            float(
-                input("Lower imaginary boundary for identity: ")
-            ),
-            float(
-                input("Upper imaginary boundary for identity: ")
-            )
-        ]
-    if pair == False:
-        domain_plot(
-            colmap,
-            f,
-            dim_Re,
-            dim_Im,
-            title,
-            sat,
-            A
-        )
-    elif pair == True:
-        pairview(
-            dim_Re,
-            dim_Im,
-            colmap,
-            f,
-            title,
-            z_dim,
-            sat,
-            A
-        )
+    mpl.tightlayout()
     settings = file_settings()
-    if settings [0] == None:
-        mpl.show()
-    else:
-        mpl.savefig(settings [0] + settings [1])
-        mpl.show()
+    mpl.savefig(settings [0] + settings [1])
+    mpl.show()
 
-user_mapping(colour_map)
+user_mapping(
+    colour_map_co,
+    colour_map_wm,
+    colour_map_ph
+)
