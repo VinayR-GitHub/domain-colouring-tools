53efabd77a4e038212594dccdb2f7da01eaef860
VinayR-GitHub <153238516+VinayR-GitHub@users.noreply.github.com>
2023-12-13 11:19:51 +1100

    Update domain_colouring_universal.py
    Removed duplicate code.

--- graphing/domain_colouring_universal.py
+++ graphing/domain_colouring_universal.py
@@ -37,7 +37,7 @@ def eval_func(f, dim_Re, dim_Im, A):
     z = x + (1j * y)
     return f(z)
 
-def colour_map(vals, sat):
+def colour_map_co(vals, sat):
     """{vals} represents a dataset of values, {sat} represents the colour saturation in the HSV system."""
     h_vals = hue(vals)
     s_vals = sat * np.ones(
@@ -188,38 +188,6 @@ def user_mapping(colmap):
         mpl.show()
 
 user_mapping(colour_map)
-#References:
-#https://notebook.community/empet/Math/DomainColoring
-#---------------
-import numpy as np
-import matplotlib.pyplot as mpl
-import matplotlib.colors as colsys
-
-def hue(z):
-    return np.mod(
-        np.angle(z) / (2 * np.pi) + 1,
-        1
-    )
-
-def eval_func(f, dim_Re, dim_Im, A):
-    """Where {f} is a function of z, {dim_Re} and {dim_Im} are lists outlining the rectangular graphing domain, and {A} represents the number of points in each unit interval."""
-    hei = dim_Im [1] - dim_Im [0]
-    len = dim_Re [1] - dim_Re [0]
-    h_res = A * hei
-    l_res = A * len
-    x = np.linspace(
-        dim_Re [0],
-        dim_Re [1],
-        int(l_res)
-    )
-    y = np.linspace(
-        dim_Im [0],
-        dim_Im [1],
-        int(h_res)
-    )
-    x, y = np.meshgrid(x, y)
-    z = x + (1j * y)
-    return f(z)
 
 def colour_map_wm(vals, sat, power = 2):
     """{vals} represents a dataset of values, {sat} represents the colour saturation in the HSV system, and {power} represents the base of the contour plot schema."""
@@ -239,24 +207,6 @@ def colour_map_wm(vals, sat, power = 2):
         (h_vals, s_vals, v_vals**0.2)
     )
     return colsys.hsv_to_rgb(col_hsv)
-
-def domain_plot(colmap, f, dim_Re, dim_Im, title = "", sat = 1, A = 500, power = 2):
-    """Where {f} is a function over z, {dim_Re} and {dim_Im} outline the dimensions of the rectangular domain, {sat} represents saturation, and {A} represents unit interval acuity."""
-    vals = eval_func(f, dim_Re, dim_Im, A)
-    cols = colmap(vals, sat, power)
-    mpl.xlabel("$\Re(z)$")
-    mpl.ylabel("$\Im(z)$")
-    mpl.title(title)
-    mpl.imshow(
-        cols,
-        origin = "lower",
-        extent = [
-            dim_Re [0],
-            dim_Re [1],
-            dim_Im [0],
-            dim_Im [1]
-        ]
-    )
 
 def pairview(dim_Re, dim_Im, colmap, f, title = "", z_dim = [-10, 10, -10, 10], sat = 1, A = 500, power = 2):
     """Where {f} is a function over z, {dim_Re}, {dim_Im}, and {z_dim} outline the dimensions of the rectangular domain, {sat} represents saturation, {A} represents unit interval acuity."""
@@ -284,22 +234,6 @@ def pairview(dim_Re, dim_Im, colmap, f, title = "", z_dim = [-10, 10, -10, 10], 
         power
     )
     mpl.tight_layout()
-
-def file_settings():
-    if bool(
-        input("Save file: ")
-    ) == False:
-        return [
-            None,
-            None
-        ]
-    else:
-        file_name = input("File name: ")
-        file_type = "." + input("File type: ") #Do not use full stop in file type.
-    return [
-        file_name,
-        file_type
-    ]
 
 def user_mapping(colmap):
     """Where {colmap} defines a colour mapping schema."""
@@ -384,46 +318,8 @@ def user_mapping(colmap):
         mpl.show()
 
 user_mapping(colour_map_wm)
-#References:
-#https://notebook.community/empet/Math/DomainColoring
-#---------------
-import numpy as np
-import matplotlib.pyplot as mpl
-import matplotlib.colors as colsys
 
-def hue(z):
-    return np.mod(
-        np.angle(z) / (2 * np.pi) + 1,
-        1
-    )
-
-def absolute_grading(z):
-    grad = lambda x: (1 - 1 / (1 + x**2))**(1 / (1 + np.pi))
-    return grad(
-        np.absolute(z)
-    )
-
-def eval_func(f, dim_Re, dim_Im, A):
-    """Where {f} is a function of z, {dim_Re} and {dim_Im} are lists outlining the rectangular graphing domain, and {A} represents the number of points in each unit interval."""
-    hei = dim_Im [1] - dim_Im [0]
-    len = dim_Re [1] - dim_Re [0]
-    h_res = A * hei
-    l_res = A * len
-    x = np.linspace(
-        dim_Re [0],
-        dim_Re [1],
-        int(l_res)
-    )
-    y = np.linspace(
-        dim_Im [0],
-        dim_Im [1],
-        int(h_res)
-    )
-    x, y = np.meshgrid(x, y)
-    z = x + (1j * y)
-    return f(z)
-
-def colour_map(vals, sat):
+def colour_map_ph(vals, sat):
     """{vals} represents a dataset of values, {sat} represents the colour saturation in the HSV system."""
     h_vals = hue(vals)
     m_val = 0.7
@@ -447,24 +343,6 @@ def colour_map(vals, sat):
         (h_vals, s_vals, v_vals)
     )
     return colsys.hsv_to_rgb(col_hsv)
-
-def domain_plot(colmap, f, dim_Re, dim_Im, title = "", sat = 1, A = 500):
-    """Where {f} is a function over z, {dim_Re} and {dim_Im} outline the dimensions of the rectangular domain, {sat} represents saturation, and {A} represents unit interval acuity."""
-    vals = eval_func(f, dim_Re, dim_Im, A)
-    cols = colmap(vals, sat)
-    mpl.xlabel("$\Re(z)$")
-    mpl.ylabel("$\Im(z)$")
-    mpl.title(title)
-    mpl.imshow(
-        cols,
-        origin = "lower",
-        extent = [
-            dim_Re [0],
-            dim_Re [1],
-            dim_Im [0],
-            dim_Im [1]
-        ]
-    )
 
 def pairview(dim_Re, dim_Im, colmap, f, title = "", z_dim = [-10, 10, -10, 10], sat = 1, A = 500):
     """Where {f} is a function over z, {dim_Re}, {dim_Im}, and {z_dim} outline the dimensions of the rectangular domain, {sat} represents saturation, {A} represents unit interval acuity."""
@@ -490,22 +368,6 @@ def pairview(dim_Re, dim_Im, colmap, f, title = "", z_dim = [-10, 10, -10, 10], 
         A
     )
     mpl.tight_layout()
-
-def file_settings():
-    if bool(
-        input("Save file: ")
-    ) == False:
-        return [
-            None,
-            None
-        ]
-    else:
-        file_name = input("File name: ")
-        file_type = "." + input("File type: ") #Do not use full stop in file type.
-    return [
-        file_name,
-        file_type
-    ]
 
 def user_mapping(colmap):
     """Where {colmap} defines a colour mapping schema."""
