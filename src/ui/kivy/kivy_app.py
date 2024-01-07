1b288f6c0227531b30c5c5c331ff82af5acb50df
VinayR-GitHub <vinayraghavan7@gmail.com>
2024-01-07 22:00:34 +1100

    Reset kivy_venv.
    modified:   src/ui/kivy/kivy_app.py

--- src/ui/kivy/kivy_app.py
+++ src/ui/kivy/kivy_app.py
@@ -1,15 +1,21 @@
+from kivy.garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg
 from kivy.app import App
-from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
 
+class MyApp(App):
 
-class MyApp(Widget):
-    pass
-
-
-class MyApplication(App):
     def build(self):
-        return MyApp()
+        box = BoxLayout()
+        box.add_widget(FigureCanvasKivyAgg(plt.gcf()))
+        return box
 
+import os
+import sys
 
-if __name__ == '__main__':
-    MyApplication().run()
+parent_dir = os.path.dirname(os.path.realpath(__file__))
+
+sys.path.append(parent_dir)
+
+from graphing import domain_colouring_universal
+
+MyApp().run()
